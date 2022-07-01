5eefd7825db223be8ff3db7f99b898e598954271
Arief Badal <1042170@hr.nl>
2022-07-01 21:32:53 +0200

    Add POST, DELETE and PUT request

--- Chapter_1/excercise_2/app.py
+++ Chapter_1/excercise_2/app.py
@@ -34,5 +34,46 @@ def get_recipe(recipe_id):
     return jsonify({'message': 'recipe not found'}, HTTPStatus.NOT_FOUND)
 
 
+@app.route('/recipes', methods=['POST'])
+def create_recipe():
+    data = request.get_json()
+    name = data.get('name')
+    description = data.get('description')
+    recipe = {
+        'id': len(recipes) + 1,
+        'name': name,
+        'description': description
+    }
+    recipes.append(recipe)
+    return jsonify(recipes), HTTPStatus.CREATED
+
+
+@app.route('/recipes/<int:recipe_id>', methods=['PUT'])
+def update_recipe(recipe_id):
+    recipe = next((recipe for recipe in recipes if recipe['id'] == recipe_id), None)
+    if not recipe:
+        return jsonify({'message': 'recipe not found'}), HTTPStatus.NOT_FOUND
+    data = request.get_json()
+    recipe.update(
+        {
+            'name': data.get('name'),
+            'description': data.get('description')
+        }
+    )
+    return jsonify(recipe)
+
+
+@app.route('/recipe/<int:recipe_id>', methods=['DELETE'])
+def delete_recipe(recipe_id):
+    for recipe in recipes:
+        recipes.remove(recipe)
+        return jsonify(recipes)
+    if recipe_id != recipe['id']:
+        return jsonify({'message': 'recipe not found'}), HTTPStatus.NOT_FOUND
+
+    
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
