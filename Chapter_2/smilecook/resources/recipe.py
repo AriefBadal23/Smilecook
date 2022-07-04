bd3ce51ad103cca25e3235ab1442074a75e5f4ab
Arief Badal <1042170@hr.nl>
2022-07-04 19:59:08 +0200

    Add project of Chapter 2 (excercise 6-9)

---
+++ Chapter_2/smilecook/resources/recipe.py
@@ -0,0 +1,64 @@
+from flask import request
+from flask_restful import Resource
+from http import HTTPStatus
+from models.recipe import Recipe, recipe_list
+
+
+class RecipeListResource(Resource):
+    """ Getting all the public recipes back"""
+    def get(self):
+        data = []
+        for recipe in recipe_list:
+            if recipe.is_publish is True:
+                data.append(recipe.data)
+        return {'data': data}, HTTPStatus.OK
+
+    def post(self):
+        """ Creates a new recipe """
+        data = request.get_json()
+        recipe = Recipe(name=data['name'],
+                 description=data['description'],
+                 num_of_servings=data['number_of_servings'],
+                 cook_time=data['cook_time'],
+                 directions=data['directions'])
+        recipe_list.append(recipe)
+        return recipe.data, HTTPStatus.CREATED
+
+class RecipeResource(Resource):
+    def get(self, recipe_id):
+        """ Retrieves one recipe according to recipe_id """
+        recipe = next((recipe for recipe in recipe_list if recipe_id == recipe_id and recipe.is_publish==True), None)
+        if recipe is None:
+            return {'message': 'recipe not found'}, HTTPStatus.NOT_FOUND
+        return recipe.data, HTTPStatus.OK
+
+    def put(self, recipe_id):
+        """ Updates the attributes of the recipe """
+        # Parses(process of converting) the incoming JSON request data and returns it
+        # Takes the incoming JSON object and converts it to a Python object in this case a dictionary
+        data = request.get_json()
+        recipe = next((recipe for recipe in recipe_list if recipe.id == recipe_id), None)
+        if recipe is None:
+            return {'message': 'recipe not found'}, HTTPStatus.NOT_FOUND
+        recipe.name = data['name']
+        recipe.desciption = data['description']
+        recipe.num_of_survings = data['num_of_servings']
+        recipe.cook_time = data['cook_time']
+        recipe.directions = data['directions']
+        return recipe.data, HTTPStatus
+
+class RecipePublishResource(Resource):   
+    """ Will update the publish status of the recipe """
+    def put(self, recipe_id):
+        recipe = next((recipe for recipe in recipe_list if recipe.id == recipe_id), None)
+        if recipe is None:
+            return {'message': 'recipe not found'}, HTTPStatus.NOT_FOUND
+        recipe.is_publish = True
+        return {}, HTTPStatus.NO_CONTENT
+    
+
+    def delete(self, recipe_id):
+        recipe = next((recipe for recipe in recipe_list if recipe.id == recipe_id), None)
+        if recipe is None:
+            return {'message': 'recipe not found'}, HTTPStatus.NOT_FOUND
+        recipe.is_publish = False
