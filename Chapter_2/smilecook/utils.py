df42aaca8546b3b1d46471efe6adf1d68b46c338
Arief Badal <1042170@hr.nl>
2022-08-02 21:14:54 +0200

    Add generate and verify user activation token (exc. 42)

--- Chapter_2/smilecook/utils.py
+++ Chapter_2/smilecook/utils.py
@@ -1,10 +1,33 @@
 from passlib.hash import pbkdf2_sha256
+from itsdangerous import URLSafeTimedSerializer
+from flask import current_app
+
+
+def generate_token(email, salt=None):
+    """ Generates a token via email for the user account  """
+    # Verify the token by using the secret key (with a timestamp)
+    serializer = URLSafeTimedSerializer(current_app.config.get('SECRET_KEY'))
+    return serializer.dumps(email, salt=salt)
+
+
+def verify_token(token, max_age=(30*60), salt=None):
+    """ Method which will extract the email address from the token,
+        which will confirm wheter the valid period in the token is
+         within 30 minutes (30*60 seconds) """
+    serializer = URLSafeTimedSerializer(current_app.config.get('SECRET_KEY'))
+    try:
+        email = serializer.loads(token, max_age=max_age, salt=salt)
+    except:
+        return False
+    return email
 
 
 def hash_password(password):
     """ Takes care of the password hashing """
     return pbkdf2_sha256.hash(password)
 
+
 def check_password(password, hashed):
-    """ Used for user authentication by comparing password and hash in the db """
+    """ Used for user authentication by comparing password and hash
+         in the db """
     return pbkdf2_sha256.verify(password, hashed)
