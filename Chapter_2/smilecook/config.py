22684d01fb02156d29749daa89e91c81c080f10b
Arief Badal <1042170@hr.nl>
2022-08-07 00:08:29 +0200

    Hide secrets by using environment variables

--- Chapter_2/smilecook/config.py
+++ Chapter_2/smilecook/config.py
@@ -1,11 +1,13 @@
+import os
+
 class Config:
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://arief:lyric_lice_clippers@localhost/smilecook"
+    SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg2://{os.getenv('DB_USERNAME')}:{os.getenv('DB_PASSWORD')}@localhost/smilecook"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_BLACKLIST_ENABLED = True
     JWT_BLACKLIST_TOKEN_CHECKS = ['access', 'refresh']
 
     # JWT key
-    SECRET_KEY = 'super-secret-key'
+    SECRET_KEY = os.getenv('SECRET_KEY')
     JWT_ERROR_MESSAGE_KEY = 'message'
     
