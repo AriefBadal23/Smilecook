f307ef0fce8d5e3b804f68590e737d9f1b1ba95e
Arief Badal <1042170@hr.nl>
2023-01-16 08:58:23 +0100

    Add environment variables

--- config.py
+++ config.py
@@ -32,11 +32,11 @@ class DevelopmentConfig(Config):
 class ProductionConfig(Config):
     SECRET_KEY = os.environ.get('SECRET_KEY')
     # SQLALCHEMY_DATABASE_URI = os.environ.get('DATATBASE_URI')
-    hostname = os.environ(os.environ['DB_HOST'])
+    hostname = os.environ['DB_HOST']
     DB_USER = os.environ['DBUSER']
     DBNAME = os.environ['DBNAME']
     HOST = hostname + ".postgres.database.azure.com"
-    PASSWORD = os.environ('DB_PASS')
+    PASSWORD = os.environ['DB_PASS']
 
     SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg2://{DB_USER}:{PASSWORD}@{HOST}:5432/{DBNAME}"    
 
